074d8d5aabaff0c9791d1002712f48471057e794
Anton Bombin <step4321@mail.ru>
2024-04-20 17:44:19 +0300

    add lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int a = 666;
+int b = 17;
+int c = 905;
+int d = 40;
+int e = 100;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
